6191c4e4040afde131ef2d63c51952b900a8f702
morenamt <onalepeloo@live.com>
2020-10-25 21:37:45 +0800

    Access Modifiers,Getters & Setters

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 
     //the public keyword makes the properties to be accessbile outside the User Class
     //properties are basically variables.
-    
+
     public $username;
-    public $email;
+    private $email;
 
     //this contructor function enables the class to instantiated with parameters/arguments, 
     //instead of manually overwriting it after it has been created.
@@ -21,30 +21,41 @@
         //it basically points to properties and methods contained in that class.
         return "$this->username Added a friend";
     }
+
+    //getters-> used to retrieve properties whose access is set to private outside the class
+    public function getEmail(){
+        return $this->email;
+    }
+
+
+    //setters-> used to update a class/object property whose access is private 
+    //from outside the class local scope
+    public function setEmail($email){
+        //by this function we want to update the email
+        if(strpos($email, "@") > -1){
+            $this->email = $email;
+
+        }
+    }
+
   }
 
-  //this is called instantiation, they are just instances of the User Class
-  //uses the construct magic method to create the objects with arguments
-  $userOne = new User("Morena","Morena@tswanakart.com");
-  $userTwo = new User("Otsogile","onalepeloo@live.com");
-
-  //to access a class properties from its instance we use an arrow ->
-  echo $userOne->username . "<br>";
-  echo $userOne->email . "<br>";
-
-  echo $userOne->addFriend() . "<br>";
+//this is called instantiation, they are just instances of the User Class
+//uses the construct magic method to create the objects with arguments
+$userOne = new User("Morena","Morena@tswanakart.com");
+$userTwo = new User("Otsogile","onalepeloo@live.com");
 
 
-  echo $userTwo->username . "<br>";
-  echo $userTwo->email . "<br>";
-  echo $userTwo->addFriend() . "<br>";
+$userOne->setEmail("morens@gmail.com");
 
+echo $userOne->getEmail() . "<br>";
+echo $userTwo->getEmail();
 
 ?>
 
 <html lang="en">
 <head>
-  <title>PHP OOP-> Properties and Methods</title>
+  <title>PHP OOP-> Access Modifiers,Getters & Setters</title>
 </head>
 <body>
   
